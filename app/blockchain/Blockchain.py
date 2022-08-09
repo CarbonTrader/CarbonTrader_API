0dffa28c1d56534106ba99fcafe8f18ac0e0b165
monotera <nelson.mosquera@javeriana.edu.co>
2022-08-08 20:32:20 -0500

    Resolves minor issues

--- app/blockchain/Blockchain.py
+++ app/blockchain/Blockchain.py
@@ -35,7 +35,24 @@ def main():
     blockchain.add_block([transaction1.generate_hash(),transaction2.generate_hash(),transaction5.generate_hash()])
     blockchain.add_block([transaction3.generate_hash(), transaction4.generate_hash()])
     blockchain.update_chain_file(blockchain.chain)
-
+    transactions = []
+    while True:
+        res = input("Welcome, wold you like to make a transaction? (y/n): ")
+        print(res)
+        if res != 'y':
+            exit()
+        _id = input("Transaction id: ")
+        buyer = input("Buyer: ")
+        seller = input("Seller: ")
+        carbon_trader_serial = input("Carbon_trader_serial: ")
+        transaction_type = input("Transaction_type: ")
+        transaction = Transaction(id,transaction_type,carbon_trader_serial,buyer,seller)
+        transactions.append(transaction)
+        if len(transactions) == 2:
+            print("Block closed!")
+            blockchain.add_block([trans.generate_hash() for trans in transactions])
+            blockchain.update_chain_file(blockchain.chain)
+            transactions = []
 
 if __name__ == '__main__':
     main()
