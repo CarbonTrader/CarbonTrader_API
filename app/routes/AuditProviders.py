8bd864131918b500384640cd1d345ae58fbaf4fd
monotera <nelson.a.mosquera@gmail.com>
2022-10-14 01:25:51 -0500

    FailSafe

--- app/routes/AuditProviders.py
+++ app/routes/AuditProviders.py
@@ -1,8 +1,8 @@
+from app.services.AuditService import AuditService
 from app.services.TransactionService import TransactionService
 from fastapi import APIRouter, HTTPException, Body
 from app.services.RecoveryService import RecoveryService
 from typing import List
-
 router = APIRouter(
     prefix="/audit",
     tags=["audit"],
@@ -12,4 +12,10 @@ router = APIRouter(
 
 @router.post("/")
 async def begin_audit(params: dict = Body(...)):
-    TransactionService.begin_audit(params)
+    try:
+        auditor = AuditService()
+        auditor.begin_audit(params)
+    except:
+        return {"asd":"asd"}
+
+
